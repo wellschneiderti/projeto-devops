1a92640fd0b35c1f18166c09139defc7d447a93b
wellschneiderti <wellschneider@yahoo.com>
2026-04-27 22:34:02 -0300

    mm

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0,1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0,2000)}
